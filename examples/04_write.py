d82f33c2e637d5daf4a9d70ad56d3d53627b0bcd
Paul Richter <32306396+richtepa@users.noreply.github.com>
2022-11-23 07:40:52 +0100

    Corrected variableName from livebit to do1

--- examples/04_write.py
+++ examples/04_write.py
@@ -5,6 +5,9 @@ writer = Client("opc.tcp://10.129.4.73:4840")
 writer.connect()
 writer.load_type_definitions()
 
-Livebit2DuoMix = writer.get_node("ns=4;s=|var|B-Fortis CC-Slim S04.Application.GVL_OPC.reserve_DO_1")
-Livebit2DuoMix.set_value(ua.Variant(True, ua.VariantType.Boolean))
+DO_1_node = writer.get_node("ns=4;s=|var|B-Fortis CC-Slim S04.Application.GVL_OPC.reserve_DO_1")
+
+# turning DO1 on
+DO_1_node.set_value(ua.Variant(True, ua.VariantType.Boolean))
+
 print("done")
